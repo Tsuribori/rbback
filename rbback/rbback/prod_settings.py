61aeb16e7fb32c119a25154be44ecb904d06ef1e
Tsuribori <tsuribori@tutanota.com>
2019-08-31 17:52:58 +0300

    Database settings for Docker

--- rbback/rbback/prod_settings.py
+++ rbback/rbback/prod_settings.py
@@ -1,15 +1,17 @@
 from rbback.settings import *
+from get_docker_secret import get_docker_secret
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '.rasberryheaven.com']
+ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'NAME': 'rbback',
+        'USER': 'rbback',
+        'PASSWORD': get_docker_secret(
+            'POSTGRES_PASSWORD', autocast_name=False),
         'HOST': 'database',
         'PORT': '5432',
         'TEST': {
