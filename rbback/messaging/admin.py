1b986447e670b8065c37be8e0f923be1ba8f3eeb
Tsuribori <tsuribori@tutanota.com>
2019-12-08 19:17:53 +0200

    Add messaging models to admin site

--- rbback/messaging/admin.py
+++ rbback/messaging/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from messaging.models import Media, Thread, Message
 
-# Register your models here.
+
+class MediaAdmin(admin.ModelAdmin):
+    search_fields = ['media_id']
+
+
+admin.site.register(Media, MediaAdmin)
+admin.site.register(Thread)
+admin.site.register(Message)
